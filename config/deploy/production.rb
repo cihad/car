c0e5e6c56dd101b722b257bf1e20ac3699f75cbb
Cihad Paksoy <cihadp@gmail.com>
2018-10-01 19:57:04 +0300

    ssl sertifikasi eklendi

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@
 
 server  "oto.teklifal.com",
         user: "deployer",
-        roles: %w{sudo app db web}
+        roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
